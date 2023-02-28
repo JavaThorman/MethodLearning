e84256e16a21a754641c30a4cd58f2dea8be6b17
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-28 13:29:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args)  {
+        System.out.println("Hello world!");
+
+        // Såjaaaa  Används github desktop, det gör livet lite enklare!
+    }
+}
